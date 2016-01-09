e8268063cd23465c90f73405362e1f9d05d41cf0
Prayag Parmar <prayagparmar@gmail.com>
2016-01-09 13:17:40 -0800

    Lesson 1 - Complete

--- app/src/main/java/com/example/prayagparmar/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/example/prayagparmar/sunshine/MainActivityFragment.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +22,21 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        List<String> forecastEntries = new ArrayList<>();
+        forecastEntries.add("Today - Sunny - 88/63");
+        forecastEntries.add("Tomorrow - Foggy - 70/46");
+        forecastEntries.add("Weds - Cloudy - 72/63");
+        forecastEntries.add("Thurs - Rainy - 64/51");
+        forecastEntries.add("Fri - Foggy - 70/46");
+        forecastEntries.add("Sat - Sunny - 76/68");
+
+        ArrayAdapter<String> populateData = new ArrayAdapter<String>(getActivity(),
+                R.layout.list_item_forecast, R.id.list_item_forecast_textview, forecastEntries);
+
+        ListView listView = (ListView) view.findViewById(R.id.listview_forecast);
+        listView.setAdapter(populateData);
+
+        return view;
     }
 }
